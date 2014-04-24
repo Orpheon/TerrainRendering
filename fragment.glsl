12d5d52b06b432d75077fc20c09d43e766e67363
Orpheon <alex.liebendorf@gmail.com>
2014-04-24 14:43:23 +0200

    Added frag shader effect to aid debugging

--- fragment.glsl
+++ fragment.glsl
@@ -1,7 +1,9 @@
 #version 330
 
+varying vec4 position;
+
 out vec4 outputColor;
 void main()
 {
-   outputColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);
+   outputColor = vec4(1.0f, 1.0 - 2.0*position.y, 1.0 - 2.0*position.y, 1.0f);
 }
--- vertex.glsl
+++ vertex.glsl
@@ -4,7 +4,10 @@ layout(location = 0) in vec4 vertex_position;
 uniform mat4 projection_matrix;
 uniform mat4 view_matrix;
 
+varying vec4 position;
+
 void main()
 {
+    position = vertex_position;
     gl_Position = view_matrix * projection_matrix * vertex_position;
 }
